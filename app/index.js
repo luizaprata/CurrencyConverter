18ee6274a802102a86b3f12b0cdac7fb5bd30011
Luiza Prata Soldi Passos <luiza.passos@lambda3.com.br>
2018-06-13 17:20:41 -0300

    styling FlatList

--- app/index.js
+++ app/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import Home from './screens/Home'
+import CurrencyList from './screens/CurrencyList'
 import EStyleSheet from 'react-native-extended-stylesheet'
 
 EStyleSheet.build({
@@ -7,9 +7,10 @@ EStyleSheet.build({
     $white:'#fff',
     $border: '#e2e2e2',
     $inputText: '#797979',
-    $lightGrey:'#f0f0f0'
+    $lightGrey:'#f0f0f0',
+    $darkText: '#343434'
 })
 
 export default () => (
-    <Home/>
+    <CurrencyList/>
 )
--- app/screens/CurrencyList.js
+++ app/screens/CurrencyList.js
@@ -1,21 +1,31 @@
-import React, { Component } from 'react';
-import { FlatList, Text, StatusBar, View } from 'react-native';
+import React, { Component } from "react";
+import { FlatList, StatusBar, View } from "react-native";
+import { ListItem, Separator } from '../components/List';
 
-import currencies from '../data/currencies';
+import currencies from "../data/currencies";
+
+const TEMP_CURRENT_CURRENCY = "CAD";
 
 class CurrencyList extends Component {
   handlePress = () => {
-    console.log('row press');
+    console.log("row press");
   };
 
   render() {
     return (
       <View style={{ flex: 1 }}>
-        <StatusBar translucent={false} barStyle="default" />
+        <StatusBar translucent={false} barStyle="light-content" />
         <FlatList
           data={currencies}
-          renderItem={({ item }) => <Text>{item}</Text>}
+          renderItem={({ item }) => (
+            <ListItem
+              text={item}
+              selected={item === TEMP_CURRENT_CURRENCY}
+              onPress={this.handlePress}
+            />
+          )}
           keyExtractor={item => item}
+          ItemSeparatorComponent={Separator}
         />
       </View>
     );
