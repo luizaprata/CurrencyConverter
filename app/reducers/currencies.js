a61dab9dd5e6ab40e2d0d3d5748230c101aa5fb6
Luiza Prata Soldi Passos <luiza.passos@lambda3.com.br>
2018-06-15 18:27:45 -0300

    integra troca moeda

--- app/reducers/currencies.js
+++ app/reducers/currencies.js
@@ -1,4 +1,9 @@
-import { CHANGE_CURRENCY_AMOUNT, SWAP_CURRENCY, CHANGE_BASE_CURRENCY, CHANGE_QUOTE_CURRENCY } from "../actions/currencies";
+import {
+  CHANGE_CURRENCY_AMOUNT,
+  SWAP_CURRENCY,
+  CHANGE_BASE_CURRENCY,
+  CHANGE_QUOTE_CURRENCY
+} from "../actions/currencies";
 
 // const initialState = {
 //   baseCurrency: 'USD',
@@ -53,6 +58,24 @@ const initialState = {
   }
 };
 
+const setConversions = (state, action) => {
+  let conversion = {
+    isFetching: true,
+    date: "",
+    rates: {}
+  };
+
+  const item = state.conversions[action.currency]
+  if (item) {
+    conversion = item;
+  }
+
+  return {
+    ...state.conversions,
+    [action.currency]: conversion
+  };
+};
+
 export default (state = initialState, action) => {
   switch (action.type) {
     case CHANGE_CURRENCY_AMOUNT:
@@ -66,14 +89,16 @@ export default (state = initialState, action) => {
     case CHANGE_BASE_CURRENCY:
       return {
         ...state,
-        baseCurrency: state.currency,
+        baseCurrency: action.currency,
+        conversions: setConversions(state, action)
       };
     case CHANGE_QUOTE_CURRENCY:
       return {
         ...state,
-        quoteCurrency: state.currency,
+        quoteCurrency: action.currency,
+        conversions: setConversions(state, action)
       };
     default:
       return state;
   }
-}
+};
